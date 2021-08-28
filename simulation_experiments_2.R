8bd143a51abc33aadba8a5e212f7d6d4ceca68ea
hayoungshin1 <64515255+hayoungshin1@users.noreply.github.com>
2021-08-28 20:43:18 +0900

    Update simulation_experiments_2.R

--- simulation_experiments_2.R
+++ simulation_experiments_2.R
@@ -18,7 +18,7 @@ L <- 1024
 # estimator <- 'huber'
 estimator <- 'tukey'
 
-n <- 8
+h <- 8
 
 true_p <- numeric(embed)
 true_p[1] <- 1
@@ -43,9 +43,9 @@ p_variance <- vector(length = length(sigmas))
 for (number in 1:length(sigmas)) {
   all_p <- matrix(, nrow = embed, ncol = L)
   for (j in 1:L) {
-    train_y <- matrix(, nrow = embed, ncol = 2 ^ n)
-    train_sims <- rnormtangents(manifold, N = 2 ^ n, n = dim, sigma_sq = sigmas[number] ^ 2)
-    for (i in 1:2 ^ n) {
+    train_y <- matrix(, nrow = embed, ncol = 2 ^ h)
+    train_sims <- rnormtangents(manifold, N = 2 ^ h, n = dim, sigma_sq = sigmas[number] ^ 2)
+    for (i in 1:2 ^ h) {
       train_y[, i] <- exp_map(manifold, true_p, train_sims[, i])
     }
     ans <- intrinsic_location(manifold, train_y, estimator, c = c, max_iter = 10000)
